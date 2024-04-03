1dbe65d6592a002926597e15a974341db8d0ee01
Akhil.Malek <akhil.malek@archetypegrowth.com>
2024-04-03 16:38:17 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,14 +23,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="max-w-2xl mx-auto py-10 px-4">
+          <div className="max-w-4xl mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
-                <ModeToggle />
+                <div className="nav-barnd">
+                <Link href="/" className="font-bold	text-xl	">Logo</Link>
+                </div>
+               
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
                   <Link href="/about">About</Link>
+                  <Link href="/contact">Contact</Link>
                 </nav>
+                <div className="px-4">
+                <ModeToggle  />
+                </div>
               </div>
             </header>
             <main>{children}</main>
